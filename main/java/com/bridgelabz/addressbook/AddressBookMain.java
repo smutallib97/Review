32bc48680e872a296952bb0c3f484d75f145442f
smutallib97 <smutallib97@gmail.com>
2022-11-22 19:22:05 +0530

    Added: Welcome to Address Book Program

---
+++ main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO ADDRESS BOOK SYSTEM");
+    }
+}
